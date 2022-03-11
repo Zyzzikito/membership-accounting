174dfc4c4702abc658620246dfbaaefc02737a37
B4bro4ka <98108675+Zyzzikito@users.noreply.github.com>
2022-03-11 23:01:22 +0300

    Update clients.py

--- screens/clients.py
+++ screens/clients.py
@@ -26,22 +26,11 @@ class Clients(Screen):
       
       
     def id_abonement_list(self):
-        id = '1i1XjLemtPa9Pj3nIYFXMVyINXoPZkq0qblnaMoVLZeY'
-        gs = gspread.service_account(filename='fileapi.json')
+        id = '' #Тут ID таблицы
+        gs = grspread.service_account(filename='fileapi.json')
         sh = gs.open_by_key(id)
         worksheet = sh.get_worksheet(1)
-        Id_abonement = worksheet.col_values(1)
-        Id_client = worksheet.col_values(2)
-        Task_abonement = worksheet.col_values(6)
-        Last_ab = worksheet.col_values(7)
-        abonementy = worksheet.col_values(5)
-        Abonement_list = {}
-        for i in Id_abonement:
-            if i == 'Идентификатор абонемента':
-                continue
-            i = int(i)
-            Abonement_list[Last_ab[i]] = Id_abonement[i] + '-' + Id_client[i] + Task_abonement[i] + '/' + abonementy[i]
-            return Abonement_list
-
+        abonement_list = worksheet.get_all_records()
+        return abonement_list
 
    
